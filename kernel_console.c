1376f7c73d32a91f1e0456214a9c82a1cd201e39
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-22 13:15:03 -0500

    Update kernel_console.c

--- kernel_console.c
+++ kernel_console.c
@@ -15,3 +15,47 @@ void kernel_console_print( const char* message ) {
      printf( message );
 }
 
+/*kernel console print address*/
+void kernel_console_print_address( void* kcpa ) {
+     printf("%p", &kcpa);
+}
+
+struct k_console {
+     int status;
+     int line;
+     char* ccmd[32];
+};
+
+struct k_usr {
+     const char* kusrn;
+     const char* kusrp;
+     struct k_console* usr_console;
+};
+
+void kernel_print_console_login_crd_failed( struct k_console* kC, struct k_usr* kU ) {
+     for(;; kC && kU ) {
+          kernel_console_print("Login Credentials Are Invalid\n");
+     }
+}
+
+#define DEF_CONSOLE_LINE 1
+
+void kernel_connect_user_console( struct k_console* c, struct k_usr* usr ) {
+     usr->usr_console = c;
+}
+
+void kernel_disconnect_user_console( struct k_console* c, struct k_usr* usr ) {
+     usr->usr_console != c; usr->usr_console = NULL;
+}
+
+void kernel_set_console_status( struct k_console* kC, struct k_usr* usr ) {
+     if( kC->status == KERNEL_CONSOLE_STATUS_CONNECTED ) {
+          kernel_connect_user_console( kC, usr );
+          kC->line = DEF_CONSOLE_LINE;
+     }
+     if( kC->status == KERNEL_CONSOLE_STATUS_DISCONNECTED ) {
+          kernel_disconnect_user_console( kC, usr );
+          kC->line = NULL;
+          kC->ccmd = NULL;
+     }
+}
