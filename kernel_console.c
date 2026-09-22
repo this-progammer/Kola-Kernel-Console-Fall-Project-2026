3402c4146dbf97923e8ddfd662036049dcd45b6a
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-22 15:51:29 -0500

    Update kernel_console.c

--- kernel_console.c
+++ kernel_console.c
@@ -20,6 +20,7 @@ void kernel_console_print_address( void* kcpa ) {
      printf("%p", &kcpa);
 }
 
+/*console*/
 struct k_console {
      int status;
      int line;
@@ -27,33 +28,40 @@ struct k_console {
      int asmmode;
 };
 
+/*user*/
 struct k_usr {
      const char* kusrn;
      const char* kusrp;
      struct k_console* usr_console;
 };
 
+/*print login credentials failed*/
 void kernel_print_console_login_crd_failed( struct k_console* kC, struct k_usr* kU ) {
      for(;; kC && kU ) {
           kernel_console_print("Login Credentials Are Invalid\n");
      }
 }
 
+/*default console line*/
 #define DEF_CONSOLE_LINE 1
 
+/*connect user to console*/
 void kernel_connect_user_console( struct k_console* c, struct k_usr* usr ) {
      usr->usr_console = c;
 }
 
+/*disconnect user from console*/
 void kernel_disconnect_user_console( struct k_console* c, struct k_usr* usr ) {
      usr->usr_console != c; 
      usr->usr_console = NULL;
 }
 
+/*kernel print that console is connected*/
 void kernel_print_console_connected() {
      kernel_console_print("Kernel Console Connected\n");
 }
 
+/*set kernel console current status*/
 void kernel_set_console_status( struct k_console* kC, struct k_usr* usr ) {
      if( kC->status == KERNEL_CONSOLE_STATUS_CONNECTED ) {
           kernel_connect_user_console( kC, usr );
@@ -71,6 +79,7 @@ void kernel_set_console_status( struct k_console* kC, struct k_usr* usr ) {
 #define KERNEL_USER_PAGE_USERNAME 0
 #define KERNEL_USER_PAGE_PASSWORD 1
 
+/*set user credential info*/
 void kernel_set_user_credential( struct k_usr* usr, int page, const char* info ) {
      if( page == KERNEL_USER_PAGE_USERNAME ) {
           const char* usrn = info;
@@ -86,27 +95,43 @@ void kernel_set_user_credential( struct k_usr* usr, int page, const char* info )
      }
 }
 
+/*kernel console enable commands*/
 void kernel_console_enable_commands( struct k_console* kC ) {
      const char* c_cmd_usr = "usr";
      const char* c_cmd_print = "print";
      const char* c_cmd_logout = "logout";
 }
 
+/*kernel asm commands*/
 const char* kernel_console_asm_list[] = {
       return "kmov", "ax", "ah"
 };
 
+/*kernel console set assembly mode*/
 int kernel_console_set_assembly_mode( struct k_console* kC ) {
      int* nMode = kC->asmmode;
      nMode = 1;
    return nMode;
 }
 
+/*kernel console host server*/
 void kernel_console_host_server( struct k_console* kC, static double ip ) {
      for(;;kC){
           *ip;
+          printf("%d\n", ip);
+          kernel_console_print_address( &kC );
      }
 }
+
+/*kernel console configure router page*/
+struct kern_console_config_rtr_pg {
+};
+
+/*kernel call to bios page*/
+struct kernel_call_to_bios_page{
+     int pgbit;
+     char biosaddr[16];
+};
 
 int main() {
      static struct k_console * kc = {
