0ba347865d94946e53b6ef43226023108e7dc5b8
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-22 12:55:13 -0500

    Update kernel_console.c

--- kernel_console.c
+++ kernel_console.c
@@ -5,3 +5,13 @@
 
 /*max login attempts*/
 #define K_MAX_ATTMPS 3
+
+/*kernel console status*/
+#define KERNEL_CONSOLE_STATUS_CONNECTED 1
+#define KERNEL_CONSOLE_STATUS_DISCONNECTED 0
+
+/*print*/
+void kernel_console_print( const char* message ) {
+     printf( message );
+}
+
